b89adaef31eebf52aad27c080312f1a982a1abb7
holmstead <65862457+holmstead@users.noreply.github.com>
2024-03-14 16:39:27 +0000

    add load csv and print dataframe summary and datatypes

--- analysis.py
+++ analysis.py
@@ -3,3 +3,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
+
+# load local csv into a pandas dataframe
+df = pd.read_csv("iris.csv")
+
+# print head and tail of the dataframe
+print(df) 
+
+# view datatypes
+print(df.dtypes) 
+
