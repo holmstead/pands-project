35c31183af5559ddb4724274f1f6aebf9c3834b5
Your Name <mat.holmes@outlook.com>
2024-03-18 09:59:11 +0000

    add loop in summarize_variables() method

--- analysis.py
+++ analysis.py
@@ -5,11 +5,7 @@ import matplotlib.pyplot as plt
 import os    # for checking if directory exists
 #import seaborn as sb
 
-# load local csv into a pandas dataframe
-# source:
-# https://ocw.mit.edu/courses/15-097-prediction-machine-learning-and-statistics-spring-2012/resources/iris/
-# better format source:
-# https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv
+# load local csv into a pandas dataframe [1]
 df = pd.read_csv("iris.csv")
 
 # print head and tail of the dataframe
@@ -18,18 +14,28 @@ print(df)
 # view datatypes
 print(df.dtypes) 
 
+# view headers
+print(df.head)
+
 def summarize_variables(df):
     '''
-    This function generates a csv file containing a summary (mean,
-    std dev, max, min) of each of the variables in the dataframe.
+    This function generates a text file containing a summary
+    (mean, std dev, max, min) of each of the variables in 
+    the dataframe.
     '''
 
-    # creat and open textfile
+    # list of variables
+    variables = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
+
+    # create and open text file
     with open("variables_summary.txt", "w") as f:
-        # write to textfile
+        # write to text file
         f.write(f"variable, mean, standard_deviation, max, min\n")
-        f.write(f"{'sepal_length'}, {df['sepal_length'].mean()}, {df['sepal_length'].std()}, {df['sepal_length'].max()}, {df['sepal_length'].min()}\n")
-        f.write(f"sepal_width, test, test, test")
+
+        # create loop to cycle through variables
+        for var in variables:
+            # write stats for each variable to the text file
+            f.write(f"{var}, {df[var].mean()}, {df[var].std()}, {df[var].max()}, {df[var].min()}\n")
 
 
 def plot_hist(variable):
@@ -37,8 +43,7 @@ def plot_hist(variable):
     This function plots a histogram from a given variable
     in a pandas dataframe
     '''
-    # creat a directory for plots if it doenst exist:
-    # https://www.w3schools.com/python/ref_os_makedirs.asp
+    # create a directory for plots if it doenst exist [2]
     os.makedirs("plots", exist_ok=True)
 
     # plot a histogram of the specified variable in the df
@@ -65,8 +70,8 @@ def plot_scatter(var1, var2):
     # creat a directory for plots if it doenst exist:
     os.makedirs("plots", exist_ok=True)
 
-    # now a scatterplot
-    # https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.scatter.html
+    # now a scatterplot [3]
+    # 
     x = df[var1]
     y = df[var2]
     plt.scatter(x, y)
@@ -87,3 +92,16 @@ def plot_scatter(var1, var2):
 plot_hist("sepal_length")
 plot_scatter("sepal_length", "sepal_width")
 summarize_variables(df)
+
+
+'''
+References:
+
+[1] https://ocw.mit.edu/courses/15-097-prediction-machine-learning-and-statistics-spring-2012/resources/iris/
+    https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv
+
+[2] https://www.w3schools.com/python/ref_os_makedirs.asp
+
+[3] https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.scatter.html
+
+'''
